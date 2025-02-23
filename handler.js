f9b1dfdec3b7f76c8ae93c635419375d7d9d3881
Ammar Shahab <ammar.qa@generalpharma.com>
2025-02-23 15:49:55 +0600

    git hub delete button tutorial

--- handler.js
+++ handler.js
@@ -1,3 +1,20 @@
 document.getElementById("btn").addEventListener("click", function () {
   document.getElementById("h1-title").innerText = "Ahh DOM Paisi";
 });
+
+/* <h2>Get the value from an input field</h2> */
+
+// 1. set eventHandler to a button
+document.getElementById("btn-update").addEventListener("click", function () {
+  //   console.log("bur");
+  // 2. get the value from input field
+  const nameInput = document.getElementById("name-input");
+  let value = nameInput.value;
+  console.log(Boolean(value));
+  if (!Boolean(value)) {
+    document.getElementById("name").innerText = "Input field empty";
+  } else {
+    //   3. set the value to the desired element
+    document.getElementById("name").innerText = value;
+  }
+});
